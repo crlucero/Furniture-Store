658f716ae553fcec02f472a34c80b114a9733e26
Cristian Lucero <cristianjlucero32@gmail.com>
2019-06-26 08:23:38 -0600

    Our products section dynamically rendered

--- app.js
+++ app.js
@@ -14,10 +14,50 @@ const productsDOM = document.querySelector('.products-center');
 let cart = [];
 
 // getting the products from json file
-class Products {}
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch('products.json');
+      let data = await result.json();
+
+      // holds data of items array in products.json
+      let products = data.items;
+
+      products = products.map(item => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
 // display products
-class UI {}
+class UI {
+  displayProducts(products) {
+    let result = '';
+    products.forEach(product => {
+      // backtic allows to dynamically render products
+      result += `
+         <article class="product">
+          <div class="img-container">
+            <img src=${product.image} class="product-img" />
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="fas-fa-shopping-cart">add to bag</i>
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>$${product.price}</h4>
+        </article>
+        `;
+    });
+    productsDOM.innerHTML = result;
+  }
+}
 
 // local storage
 class Storage {}
@@ -25,4 +65,7 @@ class Storage {}
 document.addEventListener('DOMContentLoaded', () => {
   const ui = new UI();
   const products = new Products();
+
+  //   get all products
+  products.getProducts().then(products => ui.displayProducts(products));
 });
